fc2e94856d2a2574e30c6db0417a8f16509bc440
Noam Gabriel Jacobson <noam.jacobson.business@gmail.com>
2018-07-10 19:02:34 +0300

    initial user empty categories localstorage handling. if categories is returns null fallback to empty array. abstracted category option. aligned ternary in render.

--- src/js/components/locations/SelectCategory.jsx
+++ src/js/components/locations/SelectCategory.jsx
@@ -6,30 +6,32 @@ export default class SelectCategory extends Component {
         this.props.handleChange(event);
     }
 
+    renderCategoryOptions = category => (
+        <option key={category.id} value={category.term}>
+            { category.term }
+        </option>
+    )
+
     renderSelectCategory = () => {
-        var categories = JSON.parse(localStorage.getItem('categories'))
+        var categories = JSON.parse(localStorage.getItem('categories')) || []
         return (
             <div>
                 <select name='selectedCategory' value={this.props.SelectCategory}
                     onChange={this.handleChange} >
                     <option value="" disabled selected>Select your Category</option>
-                    {categories.map(category =>
-                        <option key={category.id} value={category.term}>{category.term}</option>
-                    )}
+                    { categories.map(this.renderCategoryOptions) }
                 </select>
             </div>
         )
     }
 
     renderEditLocationSelect = () => {
-        var categories = JSON.parse(localStorage.getItem('categories'))
+        var categories = JSON.parse(localStorage.getItem('categories')) || []
         return (
             <div>
                 <select name='selectedCategory' value={this.props.SelectCategory}
                     onChange={this.handleChange} >
-                    {categories.map(category =>
-                        <option key={category.id} value={category.term}>{category.term}</option>
-                    )}
+                    { categories.map(this.renderCategoryOptions) }
                 </select>
             </div>
         )
@@ -38,10 +40,9 @@ export default class SelectCategory extends Component {
     render() {
         return (
             <div>
-                {this.props.editLocationSelect ?
-                    this.renderEditLocationSelect()
-                    :
-                    this.renderSelectCategory()
+                {this.props.editLocationSelect
+                    ? this.renderEditLocationSelect()
+                    : this.renderSelectCategory()
                 }
             </div>
         )
